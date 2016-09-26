52c9dbdf3cbe646ecfbccbeb5648c9352140f293
crazybeeliuzhe <zheliu@outlook.com>
2016-09-26 09:31:55 +0200

    Master

---
+++ BLOGGER/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BLOGGER.Startup))]
+namespace BLOGGER
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
